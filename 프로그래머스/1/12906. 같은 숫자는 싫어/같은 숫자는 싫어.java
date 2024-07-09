ef74573e31a534368e0cf9c2e93928e449e8cf64
최민서 <133588297+cmsxi@users.noreply.github.com>
2024-07-09 15:39:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.85 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public List<Integer> solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for(int i = 0; i < arr.length; i++){
+            if(answer.get(answer.size()-1) != arr[i]){
+                answer.add(arr[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
