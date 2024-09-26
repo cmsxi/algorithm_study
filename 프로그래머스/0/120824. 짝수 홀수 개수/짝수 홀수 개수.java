81c4f29eec7f9b016838ad9e679a08d657ce7326
최민서 <133588297+cmsxi@users.noreply.github.com>
2024-09-27 00:13:58 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+        for(int num : num_list){
+            if(num % 2 == 1){
+                answer[1] += 1;
+            }
+            else{
+                answer[0] += 1;
+            }
+        }
+        return answer;
+    }
+}
