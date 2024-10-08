c3b390a4c0c9d961c5820ab38c984ddf9f328606
최민서 <133588297+cmsxi@users.noreply.github.com>
2024-10-08 12:25:29 +0900

    [Silver II] Title: 최대 힙, Time: 776 ms, Memory: 35104 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,28 +1,72 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static int[] heap; 
+    static int size;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
-        
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        
-        int n = scanner.nextInt(); 
+        int n = Integer.parseInt(reader.readLine()); // 입력 받을 수의 개수
+        heap = new int[n + 1]; 
+        size = 0; // 초기화
         
         for (int i = 0; i < n; i++) {
-            int x = scanner.nextInt();
+            int x = Integer.parseInt(reader.readLine());
             if (x == 0) {
-                if (maxHeap.isEmpty()) {
+                if (size == 0) {
                     System.out.println(0); // 힙이 비어있으면 0 출력
                 } else {
-                    System.out.println(maxHeap.poll()); // 최대값 출력
+                    System.out.println(removeMax());
                 }
             } else {
-                maxHeap.add(x); // 힙에 값 추가
+                insert(x); 
             }
         }
+    }
+
+    // 삽입
+    static void insert(int value) {
+        heap[++size] = value; 
+        int index = size;
         
-        scanner.close();
+       
+        while (index > 1 && heap[index] > heap[index / 2]) {
+            int temp = heap[index];
+            heap[index] = heap[index / 2];
+            heap[index / 2] = temp;
+            index /= 2; 
+        }
+    }
+
+    //삭제
+    static int removeMax() {
+        int max = heap[1]; 
+        heap[1] = heap[size--]; 
+        heapify(1); 
+        return max;
+    }
+
+    static void heapify(int index) {
+        int left = index * 2; 
+        int right = index * 2 + 1; 
+        int largest = index; 
+
+        if (left <= size && heap[left] > heap[largest]) {
+            largest = left;
+        }
+
+        if (right <= size && heap[right] > heap[largest]) {
+            largest = right;
+        }
+        
+        if (largest != index) {
+            int temp = heap[index];
+            heap[index] = heap[largest];
+            heap[largest] = temp;
+            heapify(largest);
+        }
     }
 }
